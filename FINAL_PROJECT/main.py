131b0d7c782673d96d98f09efdb496a647df6345
Hubert <witoperek@gmail.com>
2026-06-03 19:38:00 +0200

    first try

--- FINAL_PROJECT/main.py
+++ FINAL_PROJECT/main.py
@@ -1,10 +1,33 @@
-from PyQt6.QtWidgets import QApplication
-from ui import TaskManagerUI
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
 import sys
+
+class ButtonApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("CLICKER")
+        self.button1 = QPushButton("CLICK TO EARN MONEY", self)
+        self.button2 = QPushButton("Upgrade", self)
+        self.button1.clicked.connect(self.on_click)
+        self.button1.move(60, 50)
+        self.button2.clicked.connect(self.more_m)
+        self.button2.move(80, 120)
+        self.resize(300, 200)
+        self.count = 0
+        self.click_power=1
+
+    def on_click(self):
+
+        self.count=self.count+self.click_power
+        print(self.count)
+    def more_m(self):
+        self.click_power=self.click_power+1
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = TaskManagerUI()
+    window = ButtonApp()
     window.show()
     sys.exit(app.exec())
