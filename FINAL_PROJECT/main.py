c0e10604054545aca593cd9fd453fa35a29316b4
Hubert <witoperek@gmail.com>
2026-06-05 22:09:02 +0200

    2 files less spaghetti code :)

--- FINAL_PROJECT/main.py
+++ FINAL_PROJECT/main.py
@@ -1,3 +1,10 @@
 from PyQt6.QtWidgets import QApplication
-from ui import TaskManagerUI
+from ui import ButtonApp
 import sys
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ButtonApp()
+    window.show()
+    sys.exit(app.exec())
