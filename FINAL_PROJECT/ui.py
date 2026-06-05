414a5e51bfe367dad58db367926ed8389a169ec4
Hubert <witoperek@gmail.com>
2026-06-05 23:36:49 +0200

    Add icon

--- FINAL_PROJECT/ui.py
+++ FINAL_PROJECT/ui.py
@@ -1,3 +1,5 @@
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
 import sys
 
@@ -8,13 +10,16 @@ class Clicker(QWidget):
 
     def init_ui(self):
         self.setWindowTitle("CLICKER")
-        self.button1 = QPushButton("CLICK TO EARN MONEY", self)
+        self.button1 = QPushButton(self)
+        self.button1.setIcon(QIcon("Coin_video_game.png"))
+        self.button1.setIconSize(QSize(300, 300))
+        self.button1.resize(300,300)
         self.button2 = QPushButton("Upgrade", self)
         self.button1.clicked.connect(self.on_click)
-        self.button1.move(60, 50)
+        self.button1.move(300, 50)
         self.button2.clicked.connect(self.more_m)
-        self.button2.move(80, 120)
-        self.resize(300, 200)
+        self.button2.move(350, 400)
+        self.resize(900, 600)
         self.count = 0
         self.click_power=1
 
